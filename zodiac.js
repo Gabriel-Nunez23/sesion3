fcc2b4dafd6f39af9581e12306e387032c426dd5
Gabriel Nunez <cristiannunez.gr@academia.umb.edu.co>
2023-02-08 18:35:24 -0500

    Actualizacion

--- zodiac.js
+++ zodiac.js
@@ -20,25 +20,25 @@ function zodiac() {
     if ((day>=22&&month==5) || (day<=21&&month==6)) {
         document.getElementById("imagenes").src="geminis.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=23&&month==11) || (day<=21&&month==12)) {
+        document.getElementById("imagenes").src="sagitario.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=21&&month==6) || (day<=23&&month==7)) {
+        document.getElementById("imagenes").src="cancer.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=22&&month==12) || (day<=20&&month==1)) {
+        document.getElementById("imagenes").src="capricornio.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=24&&month==7) || (day<=23&&month==8)) {
+        document.getElementById("imagenes").src="leo.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=21&&month==1) || (day<=19&&month==2)) {
+        document.getElementById("imagenes").src="acuario.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=24&&month==8) || (day<=23&&month==9)) {
+        document.getElementById("imagenes").src="virgo.jpg";
     }
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
+    if ((day>=20&&month==2) || (day<=20&&month==3)) {
+        document.getElementById("imagenes").src="piscis.jpg";
     }
 }
