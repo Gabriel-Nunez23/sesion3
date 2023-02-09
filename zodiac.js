6689bc268625ebe874b1f065b832eb30cb5f58d5
Gabriel-Nunez23 <115052840+Gabriel-Nunez23@users.noreply.github.com>
2023-02-09 09:29:53 -0500

    Update zodiac.js

--- zodiac.js
+++ zodiac.js
@@ -5,40 +5,56 @@ function zodiac() {
     year = parseInt (document.formulario.year.value);
 
     var image = document.getElementById("imagenes");
-    if ((day>=21&&month==3) || (day<=20&&month==4)) {
-        document.getElementById("imagenes").src="aries.jpg";
-    }
-    if ((day>=24&&month==9) || (day<=23&&month==10)) {
-        document.getElementById("imagenes").src="libra.jpg";
-    }
-    if ((day>=21&&month==4) || (day<=21&&month==5)) {
-        document.getElementById("imagenes").src="tauro.jpg";
-    }
-    if ((day>=24&&month==10) || (day<=22&&month==11)) {
-        document.getElementById("imagenes").src="escorpio.jpg";
-    }
-    if ((day>=22&&month==5) || (day<=21&&month==6)) {
-        document.getElementById("imagenes").src="geminis.jpg";
-    }
-    if ((day>=23&&month==11) || (day<=21&&month==12)) {
-        document.getElementById("imagenes").src="sagitario.jpg";
-    }
-    if ((day>=21&&month==6) || (day<=23&&month==7)) {
-        document.getElementById("imagenes").src="cancer.jpg";
-    }
-    if ((day>=22&&month==12) || (day<=20&&month==1)) {
-        document.getElementById("imagenes").src="capricornio.jpg";
-    }
-    if ((day>=24&&month==7) || (day<=23&&month==8)) {
-        document.getElementById("imagenes").src="leo.jpg";
-    }
-    if ((day>=21&&month==1) || (day<=19&&month==2)) {
-        document.getElementById("imagenes").src="acuario.jpg";
-    }
-    if ((day>=24&&month==8) || (day<=23&&month==9)) {
-        document.getElementById("imagenes").src="virgo.jpg";
-    }
-    if ((day>=20&&month==2) || (day<=20&&month==3)) {
-        document.getElementById("imagenes").src="piscis.jpg";
-    }
+    
 }
+
+
+function determineSign(day, month) {
+  let sign = '';
+
+  switch (month) {
+    case 1:
+      sign = (day <= 19) ? 'Capricornio' : 'Acuario';
+      break;
+    case 2:
+      sign = (day <= 18) ? 'Acuario' : 'Piscis';
+      break;
+    case 3:
+      sign = (day <= 20) ? 'Piscis' : 'Aries';
+      break;
+    case 4:
+      sign = (day <= 19) ? 'Aries' : 'Tauro';
+      break;
+    case 5:
+      sign = (day <= 20) ? 'Tauro' : 'Géminis';
+      break;
+    case 6:
+      sign = (day <= 20) ? 'Géminis' : 'Cáncer';
+      break;
+    case 7:
+      sign = (day <= 22) ? 'Cáncer' : 'Leo';
+      break;
+    case 8:
+      sign = (day <= 22) ? 'Leo' : 'Virgo';
+      break;
+    case 9:
+      sign = (day <= 22) ? 'Virgo' : 'Libra';
+      break;
+    case 10:
+      sign = (day <= 22) ? 'Libra' : 'Escorpio';
+      break;
+    case 11:
+      sign = (day <= 21) ? 'Escorpio' : 'Sagitario';
+      break;
+    case 12:
+      sign = (day <= 21) ? 'Sagitario' : 'Capricornio';
+      break;
+  }
+
+  return sign;
+}
+
+const day = 5;
+const month = 3;
+const sign = determineSign(day, month);
+console.log(`Su signo zodiacal es: ${sign}`);
